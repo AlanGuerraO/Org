6f2298c8d8751125b0030d967b26c421d23103f6
AlanGuerraO <alan.guerrao@hotmail.com>
2024-12-15 11:44:16 -0600

    Mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './componentes/header/header.jsx'
+console.log(Header)
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
